e729c3186d6ba3e66028642bc138680cc29ed27c
Hamza Ahmed <hamzaahmed00@hotmail.com>
2024-05-09 17:45:27 +0500

    add constant data (further updates needed)

---
+++ src/data/constants.ts
@@ -0,0 +1,127 @@
+export const workHistory = [
+  {
+    organizationName: "CERP",
+    duration: "2022 - 2024",
+    role: "Full-Stack Developer",
+    technologies: [
+      "React",
+      "Typescript",
+      "Javascript",
+      "Tailwind",
+      "Elixir",
+      "Phoenix",
+      "Postgres",
+      "GCP",
+    ],
+    responsibilies: "",
+    image: "",
+    href: "",
+  },
+  {
+    organizationName: "CERP",
+    duration: "2021 - 2022",
+    role: "Software Developer",
+    technologies: [
+      "React",
+      "Typescript",
+      "Javascript",
+      "Tailwind",
+      "Elixir",
+      "Phoenix",
+      "Postgres",
+    ],
+    responsibilies: "",
+    image: "",
+    href: "",
+  },
+];
+
+export const projects = [
+  {
+    name: "MiSchool",
+    description:
+      "MiSchool was developed in collaboration with the CERP Education researchers to build an application specially designed to increase the management capacity of low-cost private schools. We worked in the field to iteratively design and prioritise various features with school owners, admins and teachers, ultimately building an application which synced data between different school devices in real-time, and batch processing.",
+    resposibilities: [],
+    technologies: [
+      "React",
+      "Typescript",
+      "Javascript",
+      "Tailwind",
+      "Elixir",
+      "Postgres",
+      "GCP",
+    ],
+    image: "",
+    href: "https://mischool.pk/",
+  },
+  {
+    name: "MICare",
+    description:
+      "MICare is a digital platform that transforms how Lady Health Workers (LHWs) go from house to house and sell a nutritional product called Wellma based on the condition of a woman. Wellma is designed as a supplement to conveniently provide nutrients to expecting or lactating mothers or Married Women of Reproductive Age (MWRA) who are malnourished.",
+    resposibilities: [],
+    technologies: [
+      "React",
+      "Typescript",
+      "Javascript",
+      "Tailwind",
+      "Elixir",
+      "Phoenix",
+      "Postgres",
+      "GCP",
+    ],
+    image: "",
+  },
+  {
+    name: "Infant Feeding",
+    description:
+      "A mobile application designed to empower Lady Health Workers (LHWs) to deliver reliable healthcare information and support to expecting and lactating mothers. LHWs can easily register patients, maintain detailed patient records, and apply integrated clinical algorithms for accurate diagnosis.",
+    resposibilities: [],
+    technologies: [
+      "React",
+      "Typescript",
+      "Javascript",
+      "Tailwind",
+      "Elixir",
+      "Phoenix",
+      "Postgres",
+      "GCP",
+    ],
+    image: "",
+  },
+  {
+    name: "PxD - Weather Advisory",
+    description:
+      "A web-based weather advisory and forecast system to provide custom advisories about weather patterns and farming advice. The platform was capable of launching text campaigns and generating automated calls in multiple local languages using Text to Speech conversion services.",
+    resposibilities: [],
+    technologies: [
+      "React",
+      "Typescript",
+      "Javascript",
+      "Tailwind",
+      "Elixir",
+      "Phoenix",
+      "Postgres",
+      "GCP",
+    ],
+    image: "",
+    href: "https://precisiondev.org",
+  },
+  {
+    name: "Sehat Dost",
+    description:
+      "A web-based weather advisory and forecast system to provide custom advisories about weather patterns and farming advice. The platform was capable of launching text campaigns and generating automated calls in multiple local languages using Text to Speech conversion services.",
+    resposibilities: [],
+    technologies: [
+      "React",
+      "Typescript",
+      "Javascript",
+      "Tailwind",
+      "Elixir",
+      "Phoenix",
+      "Postgres",
+      "GCP",
+    ],
+    image: "",
+    href: "https://www.sehatdost.pk/",
+  },
+];
