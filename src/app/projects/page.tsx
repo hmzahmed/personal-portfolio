94300fad1d7d51a7a619ab87a53f499e91ca90d0
Hamza Ahmed <hamzaahmed00@hotmail.com>
2024-05-11 16:50:52 +0500

    add project cards to projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,3 +1,23 @@
-export default function Projects(){
-    return <div>Project Page</div>
+import ProjectCard from "@/components/project-card";
+import { projects } from "@/data/constants";
+
+// May explore creating a version of this in 3d using three.js
+export default function Projects() {
+  return (
+    <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-4">
+      {projects.map((project, index) => {
+        return (
+          <ProjectCard
+            description={project.description}
+            href={project.href}
+            key={project.name}
+            image={project.image}
+            technologies={project.technologies}
+            name={project.name}
+            projectId={index + 1}
+          />
+        );
+      })}
+    </div>
+  );
 }
