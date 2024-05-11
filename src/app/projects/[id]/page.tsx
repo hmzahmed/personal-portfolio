b192b8b7ea67c3b72a7b9a33a3471ffec0bd0712
Hamza Ahmed <hamzaahmed00@hotmail.com>
2024-05-11 16:49:23 +0500

    create page for individual projects

---
+++ src/app/projects/[id]/page.tsx
@@ -0,0 +1,10 @@
+import { projects } from "@/data/constants";
+
+export default function ProjectDetailPage({
+  params: { id: projectId },
+}: {
+  params: { id: string };
+}) {
+  const project = projects[parseInt(projectId) - 1];
+  return <div className="">{project.name}</div>;
+}
