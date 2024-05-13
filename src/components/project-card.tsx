f80b0d763947d968a579ddf89dd64064d0d3e18a
Hamza Ahmed <hamzaahmed00@hotmail.com>
2024-05-13 15:08:41 +0500

    design tweaks for project card

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -18,7 +18,7 @@ export default function ProjectCard({
 }: Partial<ProjectCardProps> & { image: string; projectId: number }) {
   return (
     <Link href={`/projects/${projectId}`}>
-      <div className="rounded-lg flex flex-col w-full gap-4 bg-zinc-800 p-4 shadow-md">
+      <div className="rounded-lg flex flex-col w-full gap-4 group bg-zinc-800/80 hover:bg-zinc-800 transition-all p-4 shadow-md">
         <div className="w-full h-40">
           <Image
             alt={`Logo for ${name}`}
@@ -43,7 +43,7 @@ export default function ProjectCard({
 
 const Pill = ({ text }: { text: string }) => {
   return (
-    <div className="rounded-full px-2 py-1 text-xs bg-zinc-500 text-zinc-200 transition-all hover:bg-teal-600/70">
+    <div className="rounded-full px-2 py-1 text-xs bg-zinc-700 shadow-sm text-zinc-200 transition-all hover:bg-teal-600/70">
       {text}
     </div>
   );
