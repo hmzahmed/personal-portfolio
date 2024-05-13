c04dff8e995925a1f29f67288f1bfd0151609f57
Hamza Ahmed <hamzaahmed00@hotmail.com>
2024-05-13 21:25:34 +0500

    import glb

--- src/components/model.tsx
+++ src/components/model.tsx
@@ -1,21 +1,37 @@
-"use client";
-import { Canvas } from "@react-three/fiber";
-import React, { useEffect } from "react";
+import React, { act, useEffect, useRef } from "react";
+import {
+  useAnimations,
+  useGLTF,
+  useMotion,
+  useScroll,
+} from "@react-three/drei";
+import { Group } from "three";
+import { useFrame, useThree } from "@react-three/fiber";
 
-const Model = () => {
+useGLTF.preload("/abstract_sphere.glb");
+
+export default function Model() {
+  const groupRef = useRef<Group>(null);
+  const { scene, animations } = useGLTF("/abstract_sphere.glb");
+
+  const { actions } = useAnimations(animations, scene);
+
+  useThree(({ camera }) => {
+    camera.position.setZ(32);
+  });
+
   useEffect(() => {
-    console.log("HERE");
-  }, []);
-  return (
-    <Canvas>
-      <ambientLight intensity={0.1} />
-      <directionalLight color="red" position={[0, 0, 5]} />
-      <mesh>
-        <boxGeometry />
-        <meshStandardMaterial />
-      </mesh>
-    </Canvas>
-  );
-};
+    //@ts-ignore
+    actions["Take 001"].play();
+    //@ts-ignore
+    actions["Take 001"].timeScale = 0.1;
 
-export default Model;
+    //@ts-ignore
+  });
+
+  return (
+    <group ref={groupRef}>
+      <primitive object={scene} />
+    </group>
+  );
+}
